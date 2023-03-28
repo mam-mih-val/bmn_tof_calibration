c2b71b8cb95ef3e1e3f7b75d12070666a8ee9dc9
Mikhail <mam.mih.val@gmail.com>
2023-03-28 22:01:34 +0300

    Filling histograms

--- converter.cc
+++ converter.cc
@@ -2,8 +2,8 @@
 // Created by Misha on 3/28/2023.
 //
 
-void converter(std::string file_in, std::string file_out="out.root"){
-  ROOT::RDataFrame d( "TOF400", file_in );
+void converter(std::string str_file_in, std::string str_file_out="out.root"){
+  ROOT::RDataFrame d( "TOF400", str_file_in );
   auto dd = d
           .Define("plane", "TOF400Conteiner.fPlane")
           .Define("strip", "TOF400Conteiner.fStrip")
@@ -14,7 +14,45 @@ void converter(std::string file_in, std::string file_out="out.root"){
           .Define("width", "TOF400Conteiner.fWidth")
           .Define("t0", "TOF400Conteiner.fTimeT0")
           .Define("w0", "TOF400Conteiner.fWidthT0")
+          .Define("dt", "ROOT::VecOps::RVec<float> dt; for(int i=0; i<time.size(); ++i) dt.push_back(time[i]-t0[i]); return dt;")
+          .Define("linear_id",
+                  []( ROOT::VecOps::RVec<int> plane, ROOT::VecOps::RVec<int> strip ){
+            // vectorizing the matrix of [n_planes x n_strips]
+            ROOT::VecOps::RVec<int> linear{};
+            for( size_t i=0; i<plane.size(); ++i ){
+              auto p_id = plane.at(i);
+              auto s_id = strip.at(i);
+              auto l_id = p_id * 48 + s_id;
+              linear_id.push_back(l_id);
+            }
+            return linear;
+          },
+          {"plane", "strip"})
           ;
+  std::vector<std::vector<ROOT::RDF::RResultPtr<::TH2D>> h2_tof_vs_tot;
+  std::vector<std::string> cuts;
+  for( int plane_id = 0; plane_id < 20; ++plane_id ){
+    for( int strip_id = 0; strip_id < 48; ++strip_id ){
+      std::string str_coordinate = "plane"+std::to_string(plane_id)+"_strip"+std::to_string( strip_id );
+      auto l_id = plane_id * 48 + strip_id;
+      dd.Define( str_coordinate, "plane == "+std::to_string(l_id) );
+      cuts.emplace_back( str_coordinate );
+    }
+  }
 
-  dd.Snapshot("tof400", file_out,dd.GetDefinedColumnNames());
+  for( int plane_id = 0; plane_id < 20; ++plane_id ){
+    h2_tof_vs_tot.emplace_back();
+    for( int strip_id = 0; strip_id < 48; ++strip_id ){
+      std::string str_coordinate = "plane"+std::to_string(plane_id)+"_strip"+std::to_string( strip_id );
+      h2_tof_vs_tot.back().push_back( dd.Histo2D( str_coordinate.c_str(), ";T0 width;dt", 100, 30, 50, 1000, -1000, 0  ), "w0", "dt", str_coordinate );
+    }
+  }
+
+  auto file_out = std::make_unique<TFile>(TFile::Open(str_file_out.c_str(), "recreate"));
+  for( int plane_id = 0; plane_id < 20; ++plane_id ){
+    for( int strip_id = 0; strip_id < 48; ++strip_id ){
+      h2_tof_vs_tot.at(plane_id).at(strip_id)->Write();
+    }
+  }
+  file_out->Close();
 }
