c5c9df6b73244fef1b0653da80f8a96a7059adf7
Mikhail <mam.mih.val@gmail.com>
2023-05-24 18:34:09 +0300

    Wider binning

--- fill_t0_calibration_histo.cc
+++ fill_t0_calibration_histo.cc
@@ -69,12 +69,12 @@ void fill_t0_calibration_histo(std::string str_list_in, std::string str_file_out
     h1_tot.emplace_back();
     for( int strip_id = 0; strip_id < 48; ++strip_id ){
       std::string str_coordinate = "plane"+std::to_string(plane_id)+"_strip"+std::to_string( strip_id );
-      h2_tof_vs_tot.back().push_back( dd.Histo2D({str_coordinate.c_str(), ";T0 width;dt", 512, 40, 52, 1024, -540, -516}, "w0", "dt", str_coordinate) );
+      h2_tof_vs_tot.back().push_back( dd.Histo2D({str_coordinate.c_str(), ";T0 width;dt", 512, 40, 52, 1536, -552, -516}, "w0", "dt", str_coordinate) );
       h1_tot.back().push_back( dd.Histo1D({std::data("width_"+str_coordinate), ";width;counts", 100, 0, 100}, "width", str_coordinate) );
     }
   }
 
-  auto h2_tof_vs_tot_integral = dd.Histo2D({"tof_vs_tot_integral", ";T0 width;dt", 512, 40, 52, 1024, -540, -516}, "w0", "dt");
+  auto h2_tof_vs_tot_integral = dd.Histo2D({"tof_vs_tot_integral", ";T0 width;dt", 512, 40, 52, 1536, -552, -516}, "w0", "dt");
 
   auto file_out = std::make_unique<TFile>(str_file_out.c_str(), "recreate");
   for( int plane_id = 0; plane_id < 20; ++plane_id ){
